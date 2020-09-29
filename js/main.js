6bb44cd0f146c11d586260f067afb20cece7c4be
ledimari <ledi.huliganka@gmail.com>
2020-09-29 15:10:30 +0300

    added slider end footer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-function init() {
-  // функция вызовет сама себя как только сайт загрузиться
-}
-
-(init)();
+$(function(){
+$('.photo__slider').slick({
+  prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.svg" alt=""></button>',
+  nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.svg" alt=""></button>'
+});
+});
