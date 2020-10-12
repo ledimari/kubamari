d0cd06d2f5938e308823958e2e82151b47a13f99
ledimari <ledi.huliganka@gmail.com>
2020-10-12 20:55:40 +0300

    add responsive

--- js/main.js
+++ js/main.js
@@ -3,4 +3,9 @@ $('.photo__slider').slick({
   prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.svg" alt=""></button>',
   nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.svg" alt=""></button>'
 });
+
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
+
 });
